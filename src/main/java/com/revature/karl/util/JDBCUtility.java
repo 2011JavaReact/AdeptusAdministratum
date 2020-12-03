2c04b76d98080925bb550f98cffafae05528125d
CarlKarlQarl <karlmatthes@gmail.com>
2020-12-03 13:05:00 -0700

    Testing JDBC

--- src/main/java/com/revature/karl/util/JDBCUtility.java
+++ src/main/java/com/revature/karl/util/JDBCUtility.java
@@ -15,14 +15,14 @@ public class JDBCUtility {
 //		String password = System.getenv("DB_PASSWORD");
 		
 		//Amazon
-		String url = System.getenv("RDS_URL");
-		String username = System.getenv("RDS_USERNAME");
-		String password = System.getenv("RDS_PASSWORD");
+//		String url = System.getenv("RDS_URL");
+//		String username = System.getenv("RDS_USERNAME");
+//		String password = System.getenv("RDS_PASSWORD");
 
 		Connection connection = null;
 
 		DriverManager.registerDriver(new Driver());
-		connection = DriverManager.getConnection(url, username, password);
+		connection = DriverManager.getConnection("jdbc:postgresql://karl-database-2.c6ffouydqidt.us-west-1.rds.amazonaws.com:5432/postgres?user=postgres&password=livinginthedatabase");
 
 		System.out.println("Connected to database.");
 		
